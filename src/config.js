1a0c6c4b0785e4dfe349bdb8ad8ca202b71229d6
Bruno Catão <brunogamacatao@gmail.com>
2018-03-28 18:01:19 -0300

    Heroku

--- src/config.js
+++ src/config.js
@@ -55,7 +55,8 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/loja-backend'
+      uri: process.env.MONGODB_URI || 'mongodb://localhost/loja-backend',
+      options: {}
     }
   }
 }
